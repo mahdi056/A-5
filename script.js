db92638023d92586f020884a9f572c08f1440bd2
mahdi056 <ahnafmahdi12@gmail.com>
2024-09-24 19:23:25 +0600

    All donate button setup

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ document.getElementById('history-btn').addEventListener('click',function(){
     document.getElementById('show-donation').classList.add('hidden');
 
     document.getElementById('show-history').classList.remove('hidden');
+
+    document.getElementById('history-btn').classList.add('bg-primary-color');
+
+    document.getElementById('donate-btn').classList.remove('bg-primary-color');
 });
 
 
@@ -9,9 +13,15 @@ document.getElementById('donate-btn').addEventListener('click',function(){
     document.getElementById('show-history').classList.add('hidden');
 
     document.getElementById('show-donation').classList.remove('hidden');
+
+    document.getElementById('donate-btn').classList.add('bg-primary-color');
+
+    document.getElementById('history-btn').classList.remove('bg-primary-color');
+
+
 });
 
-
+// For noakhali
 document.getElementById('noakhali-btn').addEventListener('click', function(event){
     event.preventDefault();
 
@@ -30,5 +40,51 @@ document.getElementById('noakhali-btn').addEventListener('click', function(event
     let currentNoakhaliAmount = noakhaliAmount + NoakhaliInput;
 
     document.getElementById('noakhali-amount').innerText = currentNoakhaliAmount;
+
+});
+
+// For Feni 
+
+document.getElementById('feni-btn').addEventListener('click', function(event){
+    event.preventDefault();
+
+    let myTaka = parseFloat(document.getElementById('my-taka').innerText);
+
+    let feniAmount = parseFloat(document.getElementById('feni-amount').innerText);
+
+    let feniInput = parseFloat(document.getElementById('feni-input').value);
+    // My Money calculation
+    let myCurentMoney = myTaka - feniInput;
+
+    document.getElementById('my-taka').innerText = myCurentMoney;
+
+    // Feni donation calculation
+
+    let currentFeniAmount = feniAmount + feniInput;
+
+    document.getElementById('feni-amount').innerText = currentFeniAmount;
+
+});
+
+// For Quota
+
+document.getElementById('quota-btn').addEventListener('click', function(event){
+    event.preventDefault();
+
+    let myTaka = parseFloat(document.getElementById('my-taka').innerText);
+
+    let quotaAmount = parseFloat(document.getElementById('quota-amount').innerText);
+
+    let quotaInput = parseFloat(document.getElementById('quota-input').value);
+    // My Money calculation
+    let myCurentMoney = myTaka - quotaInput;
+
+    document.getElementById('my-taka').innerText = myCurentMoney;
+
+    // Feni donation calculation
+
+    let currentquotaAmount = quotaAmount + quotaInput;
+
+    document.getElementById('quota-amount').innerText = currentquotaAmount;
 
 });
